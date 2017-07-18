d210e70c6685213d1fdeae8e251d5ccccefe3d87
unknown <Administrator@SNAIR-Z620.na.corp.mckesson.com>
2017-07-17 21:57:57 -0700

    Both Exercise 1 & 2 ver1

---
+++ Program_Ex1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Robot robo = Robot.GetRobot();
+
+            //Test Sort Chip
+
+            var numbers = new int[6] {4, 5, 2, 1,6,8};
+            IChip sortChipAsc = new SortChip(Order.Ascending);
+            robo.Plug(sortChipAsc);
+
+            var output1 = robo.Execute(numbers);
+
+            Console.WriteLine("*******Ascending Chip******");
+            foreach (var n in output1)
+            {
+                Console.WriteLine(n);
+            }
+
+            IChip sortChipDesc = new SortChip(Order.Descending);
+            robo.Plug(sortChipDesc);
+
+            var output = robo.Execute(numbers);
+
+            Console.WriteLine("*******Descending Chip******");
+            foreach (var n in output)
+            {
+                Console.WriteLine(n);
+            }
+
+            IChip sumChip = new SumChip();
+            robo.Plug(sumChip);
+
+            var outputSum = robo.Execute(numbers);
+
+            Console.WriteLine("*******Sum Chip******");
+            foreach (var n in outputSum)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("*******Total Chips******");
+            Console.WriteLine(robo.NumberOfChipsInstalled);
+
+            Console.ReadLine();
+        }
+    }
+    //Used a singleton pattern , which is ideal for these kind of problems
+    public class Robot
+    {
+        private static Robot _robot;
+        private static IChip _chip;
+        private int _numberOfChipsInstalled;
+        private Robot()
+        {
+
+        }
+
+        public static Robot GetRobot()
+        {
+            return _robot ?? (_robot = new Robot());
+        }
+
+        public int[] Execute(int[] numbers)
+        {
+
+            if (_chip == null)
+            {
+                Console.WriteLine("Please Install a chip");
+                return null;
+            }
+
+
+            if (numbers != null && numbers.Length > 0)
+            {
+                return _chip.Execute(numbers);
+            }
+
+            return null;
+
+        }
+
+        public void Plug(IChip chip)
+        {
+            _chip = chip;
+            _numberOfChipsInstalled++;
+        }
+
+        public int NumberOfChipsInstalled { get { return _numberOfChipsInstalled; } }
+
+    }
+
+    public enum Order
+    {
+        Ascending,
+        Descending
+    }
+    public class SortChip : IChip
+    {
+        private Order _orderType;
+        public SortChip(Order orderType)
+        {
+            _orderType = orderType;
+        }
+        public int[] Execute(int[] numbers)
+        {
+            return Sort(numbers);
+        }
+
+        private int[] Sort(int[] numbers)
+        {
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    //could have made better with a reusable code
+                    if (_orderType == Order.Ascending)
+                    {
+                        if (numbers[i] > numbers[j])
+                        {
+                            var temp = numbers[i];
+                            numbers[i] = numbers[j];
+                            numbers[j] = temp;
+                        }
+                    }
+                    else
+                    {
+                        if (numbers[i] < numbers[j])
+                        {
+                            var temp = numbers[i];
+                            numbers[i] = numbers[j];
+                            numbers[j] = temp;
+                        }
+                    }
+
+                }
+            }
+
+            return numbers;
+        }
+
+
+
+    }
+    public class SumChip : IChip
+    {
+        public int[] Execute(int[] numbers)
+        {
+            return new int[1] { TotalSum(numbers) };
+        }
+
+        public int TotalSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (var value in numbers)
+            {
+                sum = sum + value;
+            }
+            return sum;
+
+        }
+    }
+
+    public interface IChip
+    {
+        int[] Execute(int[] numbers);
+    }
+}
---
+++ Program_Ex2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<StoreHours> st = new List<StoreHours>();
+            var st1 = new StoreHours(1, new TimeSpan(10,10,1) , new TimeSpan(21, 10,1));
+            var st2 = new StoreHours(2, new TimeSpan(10, 10, 1), new TimeSpan(21, 10, 1));
+            var st4 = new StoreHours(3, new TimeSpan(10, 10, 1), new TimeSpan(21, 10, 1));
+            var st3 = new StoreHours(4, new TimeSpan(10, 10, 1), new TimeSpan(21, 10, 1));
+            var st5 = new StoreHours(5, new TimeSpan(10, 10, 1), new TimeSpan(21, 10, 1));
+            var st6 = new StoreHours(6, new TimeSpan(10, 10, 1), new TimeSpan(21, 10, 1));
+            var st7 = new StoreHours(7);
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+    public enum Day
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday  = 4,
+        Friday =5 ,
+        Saturday = 6,
+        Sunday = 7
+
+
+    }
+    public class StoreHours
+    {
+        private int _dayOfWeek;
+        private TimeSpan  _startTime, _endTime;
+        private bool _closed;
+        public StoreHours(int dayOfWeek, TimeSpan  startTime, TimeSpan  endTime)
+        {
+            ValidateDay(dayOfWeek);
+            ValidateTimeSpan(startTime, endTime);
+            _dayOfWeek = dayOfWeek;
+            _startTime = startTime;
+            _endTime = endTime;
+
+
+        }
+
+        public StoreHours(int dayOfWeek)
+        {
+            ValidateDay(dayOfWeek);
+            _dayOfWeek = dayOfWeek;
+            _closed = true;
+        }
+
+        void ValidateDay(int day)
+        {
+            if (day < 0 || day > 7)
+            {
+                throw new Exception("Invalid day");
+            }
+        }
+
+        void ValidateTimeSpan(TimeSpan start, TimeSpan end)
+        {
+            if (start > end)
+            {
+                throw new Exception("Invalid timespan");
+            }
+        }
+
+
+    }
+}
